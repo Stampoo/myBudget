946e5cf5f30caecdc3a529ce6d42a23379aa5f91
fivecoil <fivecoil@gmail.com>
2020-07-01 12:39:40 +0300

    remove secret

--- myBudget/Service/Network/NetworkManager.swift
+++ myBudget/Service/Network/NetworkManager.swift
@@ -28,10 +28,3 @@ final class NetworkManager {
         }
     }
 }
-
-private enum Constants {
-    static let token = "f10db5b8439d9f80397cb6aedaa403af"
-    static let baseURL = "http://data.fixer.io/api/"
-    static let example = "http://data.fixer.io/api/latest?access_key=f10db5b8439d9f80397cb6aedaa403af"
-
-}
