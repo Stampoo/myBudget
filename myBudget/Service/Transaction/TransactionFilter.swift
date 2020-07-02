a55a1d0513957f584ee16369f5ad402cad782979
fivecoil <fivecoil@gmail.com>
2020-07-02 16:48:35 +0300

    add filter service

--- myBudget/Service/Transaction/TransactionFilter.swift
+++ myBudget/Service/Transaction/TransactionFilter.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+
+final class TransactionFilter {
+
+    //MARK: - Types
+
+    enum FilterType {
+        case date
+    }
+
+
+    //MARK: - Private properties
+
+    private let transactionStorage = TempHistoryStorageService.shared
+
+    //MARK: - Public methods
+
+    func filterBy(type: FilterType, to budget: Budget) -> [Transaction] {
+        switch type {
+        case .date:
+            return filterByDate(to: budget)
+        }
+    }
+
+
+    //MARK: - Private methods
+
+    private func filterByDate(to budget: Budget) -> [Transaction] {
+        var tempSlotForTransaction: Transaction
+        var transactions = transactionStorage.openHistory(budget: budget)
+        for (index, transaction) in transactions.enumerated() {
+            for (ind, trans) in transactions.enumerated() {
+                if transaction.date < trans.date {
+                    tempSlotForTransaction = transaction
+                    transactions[index] = trans
+                    transactions[ind] = tempSlotForTransaction
+                }
+            }
+        }
+        return transactions
+    }
+
+}
